bc145c9ae49ea07645940160534427591068c008
Grigorii Skovorodnikov <gskovorod3@gmail.com>
2023-02-22 19:41:11 +0500

    add dz 10

--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+System.Console.WriteLine("Imput number");
+int N = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+for (int i = 1; i <= N; i++)
+{
+    System.Console.Write(Math.Pow(i, 3) + ", ");
+}
